20a169a842ffa115af881b70fe8b10f007ea086d
frezzenboy@gmail.com <39112635+deuslux12@users.noreply.github.com>
2018-12-09 21:23:10 -0600

    Mapas y rutas

---
+++ php/ADMINISTRACION.php
@@ -0,0 +1,32 @@
+<?php
+require_once("myDBC.php");
+if(isset($_SESSION['session']))
+{
+    ?>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Pagina de mapas </title>
+    </head>
+    <body bgcolor="#708090">
+    <br><font face="Arial" size="+1" color="#FFFFFF" ><a href="salir.php"style="float:right" >Cerra Sesion</a></font></br>
+    <br> <font face="Arial" size="+1" color="#FF0000" ><a href="principal.php"style="float:right" >Regresar</a></font></br>
+    <font size="20" face ="Snap ITC" color="blue">
+        <center>
+            <header>
+                <div class="header-top">
+                    <div class="navegacion">
+                        <input type="search" placeholder="Buscar . . ." id="inputBusqueda">
+                    </div></div></center>
+        <marquee behavior =alternate>Tu edificio es: </marquee>
+        <center><img src="../imagenes/ADMINISTRACION.png"  ></center>
+        </header>
+    </body>
+    </html>
+    <?php
+}else
+    echo'<script type="text/javascript">
+      alert("Registrarse para ver este contenido");
+      window.location="http://localhost/sistema/index.php"
+</script>';
+?>
