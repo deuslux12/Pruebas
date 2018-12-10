6dcbbafedab359927985c2381fcb2ef757327771
frezzenboy@gmail.com <39112635+deuslux12@users.noreply.github.com>
2018-12-09 21:16:53 -0600

    datos carreras

--- php/principal.php
+++ php/principal.php
@@ -4,21 +4,101 @@ if(isset($_SESSION['session']))
 {
 ?>
 <!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>..:PAGINA PRINCIPAL:..</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
+<title>Buscador</title>
+<link rel="stylesheet" type="text/css" href="../css/style2.css">
 </head>
-
-<body bgcolor="#F5A9F2">
-	<center><h2>BIENVENIDO</h2></center>
-
-	<a href="salir.php"><h4>CERRAR SESSION</h4></a>
+<body bgcolor="#FFC0CB">
+<div class="row">
+<center>
+    <br><font face="Arial" size="+1" ><a href="salir.php"style="float:right" >Cerra Sesion</a></font></br>
+<div class="wrapper">
+<div id="title">
+<h1>seleccione una imagen para conocer los departamentos </h1>
+<input id="search-box" type="text" name="search">
+</div>
+<div id="directory-cont"><ul id="directory">
+<center>
+<div>
+<table>
+<tr>
+<td>
+                                        <div></div>
+                                        <div class="wrap">
+                                        <div></div>
+                                        <center>
+                                        <div><img src="ISC.jpg"></a>
+                                        <p class="name"><a href="sistemas.php">sistemas computacionales</a></p>
+                                        <p>&#9742 1111 - 1111</p>
+                                        </div>
+                                        </center>
+</td>
+<td><td><td><td><td><td><td><td><td><td><td><td><td><td><td><td></td></td></td></td></td></td></td></td></td></td></td></td></td></td></td></td>
+<td>
+                                        <center>
+                                        <div><img src="IC.jpg">
+                                        <p class="name"><a href="CIVIL.php">ingenieria civil</a></p>
+                                        <p>&#9742 2222 - 2222</p>
+                                        </div>
+                                       </center>
+</td>
+</tr>
+<tr>
+<td>
+                                        <center>
+                                        <div><img src="IGE.jpg">
+                                        <p class="name"><a href="GESTION.php">ingenieria en gestion</a></p>
+                                        <p>&#9742 3333 - 3333</p>
+                                        </div>
+                                        </center>
+</td>
+<td><td><td><td><td><td><td><td><td><td><td><td><td><td><td><td></td></td></td></td></td></td></td></td></td></td></td></td></td></td></td></td>
+<td>
+                                        <center>
+                                        <div><img src="II.jpg">
+                                        <p class="name"><a href="INDUSTRIAL.php">ingenieria industrial</a></p>
+                                        <p>&#9742 4444 - 4444</p>
+                                        </div>
+    </center>
+</td>
+</tr>
+<tr>
+<td>
+                                        <center>
+                                        <div><img src="IM.jpg">
+                                        <p class="name"><a href="MECATRONICA.php">ingenieria mecatronica</a></p>
+                                        <p>&#9742 3333 - 3333</p>
+                                        </div>
+                                        </center>
+</td>
+<td><td><td><td><td><td><td><td><td><td><td><td><td><td><td><td></td></td></td></td></td></td></td></td></td></td></td></td></td></td></td></td>
+<td>
+                                        <center>
+                                        <div><img src="admin.png">
+                                        <p class="name"><a href="ADMINISTRACION.php">Administracion</a></p>
+                                        <p>&#9742 4444 - 4444</p>
+                                        </div>
+                                        </center>
+</td>
+</tr>
+</table>
+</div>
+    </ul>
+</div>
+</center>
+</div> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="../js/jquery.liveFilter.js" type="text/javascript"></script>
+<script type="text/javascript">$('#directory').liveFilter('#search-box', 'td', { filterChildSelector: '.name'});</script>
 </body>
 </html>
 <?php
 }else
 echo'<script type="text/javascript">
-	  alert("Registrarse para ver este contenido");
-	  window.location="http://localhost/infetec/login/index.php"
+      alert("Registrarse para ver este contenido");
+      window.location="http://localhost/infetec/login/index.php"
 </script>';
 ?>
